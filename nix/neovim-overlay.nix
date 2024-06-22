1f3d34c87fae5847a2d653ec68a879f8b21d4d67
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-06-22 22:17:00 +0200

    Readd command line suggestions

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -39,8 +39,8 @@ with final.pkgs.lib; let
     cmp-buffer # current buffer as completion source | https://github.com/hrsh7th/cmp-buffer/
     cmp-path # file paths as completion source | https://github.com/hrsh7th/cmp-path/
     cmp-nvim-lua # neovim lua API as completion source | https://github.com/hrsh7th/cmp-nvim-lua/
-    # cmp-cmdline # cmp command line suggestions
-    # cmp-cmdline-history # cmp command line history suggestions
+    cmp-cmdline # cmp command line suggestions
+    cmp-cmdline-history # cmp command line history suggestions
 
     # ^ nvim-cmp extensions
     # git integration plugins
